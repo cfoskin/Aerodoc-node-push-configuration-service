b195a8dfe778c041950053b28af6008ca790ab2d
Colum Foskin <columfoskin@gmail.com>
2017-02-07 20:18:03 +0000

    added tests for endpoints

---
+++ test/pushConfigSpec.js
@@ -0,0 +1,120 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+const supertest = require('supertest');
+const app = require('../index');
+const fixtures = require('../resources/fixtures.json');
+const should = require('chai').should;
+const expect = require('chai').expect;
+const pushConfigs = fixtures.pushConfigs;
+const pushConfigUrl = '/aerodoc/rest/pushConfigs/';
+var pushConfigId;
+const mongoose = require('mongoose');
+
+describe('Push Configuration API Integration Tests', () => {
+    before((done) => {
+        mongoose.connection.collections['pushconfigs'].drop((err) => {
+            console.log('test db reset');
+        });
+        done();
+    });
+
+    it('Should not create a new Push Configuration', (done) => {
+        var badPushConfig = {
+        "masterSecret": "secret",
+        "active": "false"
+    };
+        supertest(app)
+            .post(pushConfigUrl)
+            .send(badPushConfig)
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(500);
+                done();
+            });
+    });
+
+     it('Should not create a bad Push Configuration', (done) => {
+        var pushConfig = pushConfigs[0];
+        supertest(app)
+            .post(pushConfigUrl)
+            .send(pushConfig)
+            .end((err, res) => {
+                expect(res.body).to.have.property("pushApplicationId", "test");
+                expect(res.body).to.have.property("masterSecret", "secret");
+                expect(res.body).to.have.property("serverURL", "testURL");
+                expect(res.body).to.have.property("active", false);
+                expect(res.statusCode).to.be.equal(201);
+                pushConfigId = res.body._id;
+                done();
+            });
+    });
+
+    it('Should get all Push Configs', (done) => {
+        supertest(app)
+            .get(pushConfigUrl)
+            .end((err, res) => {
+                expect(Array.isArray(res.body));
+                expect(res.statusCode).to.be.equal(200);
+                done();
+            });
+    });
+
+    it('Should get one Push Config', (done) => {
+        supertest(app)
+            .get(pushConfigUrl + pushConfigId)
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(200);
+                done();
+            });
+    });
+
+    it('Should not get no existent Push Config', (done) => {
+        supertest(app)
+            .get(pushConfigUrl + 'badId')
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(404);
+                done();
+            });
+    });
+
+    it('Should update a Push Config', (done) => {
+        supertest(app)
+            .put(pushConfigUrl + pushConfigId)
+            .send({ pushApplicationId: 'newId' })
+            .end((err, res) => {
+                let pushConfig = res.body;
+                expect(res.body).to.have.property("pushApplicationId", "newId");
+                expect(res.body).to.not.have.property("pushApplicationId", "test");
+                done();
+            });
+    });
+
+    it('Should not update a non existent Push Config', (done) => {
+        supertest(app)
+            .put(pushConfigUrl + 'badId')
+            .send({ pushApplicationId: 'newId' })
+            .end((err, res) => {
+                 expect(res.statusCode).to.be.equal(404);
+                done();
+            });
+    });
+
+    it('Should delete one Push Config', (done) => {
+        supertest(app)
+            .delete(pushConfigUrl + pushConfigId)
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(204);
+                done();
+            });
+    });
+
+     it('Should not delete non existent Push Config', (done) => {
+        supertest(app)
+            .delete(pushConfigUrl + 'badId')
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(404);
+                done();
+            });
+    });
+
+});
